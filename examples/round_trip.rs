65006134d8c4f20a23a58282b87d6e4c9daa4c87
Eyal Kalderon <ebkalderon@gmail.com>
2019-12-19 13:30:32 +0800

    Change round_trip example to compress repo itself
    This should turn this example into a more realistic benchmark for
    typical reading/writing speeds. However, since this currently includes
    a number of non-reproducible directories (most notably, `.git` and
    `target`), this example cannot yet be turned into a Criterion benchmark.
    Ideally, there should be some way to filter out files at initialization
    time, perhaps via use of the `ignore` crate and turning both
    `libnar::to_writer()` and `libnar::to_vec()` into builders instead.

--- examples/round_trip.rs
+++ examples/round_trip.rs
@@ -3,15 +3,15 @@ use std::path::Path;
 
 use libnar::Archive;
 
-const TARGET_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src");
+const TARGET_PATH: &str = env!("CARGO_MANIFEST_DIR");
 
 fn main() {
-    let nar = Path::new(TARGET_PATH).with_extension("nar");
+    let nar = Path::new(TARGET_PATH).join("libnar").with_extension("nar");
 
     let mut file = File::create(&nar).unwrap();
     libnar::to_writer(&mut file, TARGET_PATH).unwrap();
 
     let file = File::open(&nar).unwrap();
     let mut nar = Archive::new(file);
-    nar.unpack("src2").unwrap();
+    nar.unpack("libnar").unwrap();
 }
